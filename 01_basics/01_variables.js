139f5791d6c4d27c8537c24c3faf5bb13d960c29
Sonal Ghinaiya <147973094+sonalghinaiya@users.noreply.github.com>
2024-07-23 16:33:51 +0000

    #

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 1234
+let accountEmail = "ghinaiyasonal@gmail"
+var accountPassword = "12345"
+accountCity = "Ahemedabad"
+let accountState
+
+//accountId = 2 //not allowed
+accountEmail = "gsgs@gmail.com"
+accountPassword = "23232"
+accountCity = "Germany"
+
+console.log(accountId);
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountCity);
+console.log(accountState);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])

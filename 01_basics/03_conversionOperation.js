ea78eb618387924355915456b3a1bf149aec5956
Sonal Ghinaiya <147973094+sonalghinaiya@users.noreply.github.com>
2024-07-24 16:12:54 +0000

    #

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -12,9 +12,52 @@
 // console.log(valueInNumber);
 
 //let isLoggedIn = 1 =>true
+//let isLoggedIn = 0 =>false
 // let isLoggedIn = ""  =>false
  let isLoggedIn = "Sonal"  //=>true
 
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn); 
+//console.log(booleanIsLoggedIn); 
+
+let num = 33
+let StringNumber = String(num)
+
+//console.log(StringNumber)
+//console.log(typeof StringNumber)
+
+//****************** Operation *********************
+
+let value = 3
+let negValue = -value
+
+//console.log(negValue);
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 ** 3);
+// console.log(2 / 2);
+// console.log(3 % 2);
+
+let str1 = "Hello"
+let str2 = " Sonal"
+let str3 = str1 + str2
+
+// console.log(str3);
+
+// console.log("1" + 2) //12
+// console.log(1 + "2") //12
+// console.log("1"+ 2 + 2) //122
+// console.log(1 + 2 + "2") //32
+
+// console.log(+true)
+// console.log(+"")
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+
+let counter = 100
+//counter++
+++counter
+console.log(counter);

381171561c2aeb54b0c1b77e643dcfc9d7f9f6fa
Sonal Ghinaiya <147973094+sonalghinaiya@users.noreply.github.com>
2024-07-24 02:43:20 +0000

    #

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,20 @@
+// let score = "33" =>33
+//let score = "33abc" => NaN type-number
+//let score = null 
+//let score = undefined
+// let score = true //=>1 false=>0
+
+// console.log(typeof score);
+
+// //convert in number
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//let isLoggedIn = 1 =>true
+// let isLoggedIn = ""  =>false
+ let isLoggedIn = "Sonal"  //=>true
+
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn); 

747ba390d6d06751f14a22c8114e8eeb33462492
Sonal Ghinaiya <147973094+sonalghinaiya@users.noreply.github.com>
2024-07-25 13:26:16 +0000

    Number and Maths

--- 01_basics/06_num_and_maths.js
+++ 01_basics/06_num_and_maths.js
@@ -1,1 +1,38 @@
-shxh
+const score = 100
+//console.log(score);
+
+const balance = new Number(200)
+//console.log(balance);
+
+//console.log(balance.toString().length);
+//console.log(balance.toFixed(2));
+
+const num = 123.833
+//console.log(num);
+
+//console.log(num.toPrecision(3));
+
+const hundreds = 1000000
+//console.log(hundreds.toLocaleString('en-IN'));
+
+//console.log(Number.MAX_VALUE);
+//console.log(Number.MIN_VALUE);
+
+//******************** Maths ****************************
+
+// console.log(Math); //run in browser
+// console.log(Math.abs(-1));
+// console.log(Math.round(4.5));
+// console.log(Math.ceil(4.5));
+// console.log(Math.floor(4.5));
+// console.log(Math.min(40, 10, 20, 30));
+// console.log(Math.max(40, 10, 20, 30));
+
+console.log(Math.random());
+console.log((Math.random()*100));
+console.log((Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min))+min);

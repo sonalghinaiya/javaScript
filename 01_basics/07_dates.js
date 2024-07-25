dc442796b7471a923febee167b0711df183db453
Sonal Ghinaiya <147973094+sonalghinaiya@users.noreply.github.com>
2024-07-25 14:13:34 +0000

    #

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,8 @@
+let myDate = new Date()
+console.log(myDate);
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toISOString()); //toJSON Same
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toLocaleString());
+console.log(myDate.toLocaleTimeString());

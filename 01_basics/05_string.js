05c85ad924ad6efb96357589c97b309da8221b08
Sonal Ghinaiya <147973094+sonalghinaiya@users.noreply.github.com>
2024-07-25 03:19:49 +0000

    #

---
+++ 01_basics/05_string.js
@@ -0,0 +1,18 @@
+const name = "Sonal"
+const age = 20
+
+//console.log(name + age + " Hello");
+
+//console.log(`Hello my name is ${name} and my age is ${age}`);
+
+const gameName = new String("Sonal Ghinaiya")
+
+console.log(gameName);
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.toLowerCase());
+console.log(gameName.charAt(2));
+

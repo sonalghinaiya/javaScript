30244655089b4a0290eac93f5c159d7f6fd74351
Sonal Ghinaiya <147973094+sonalghinaiya@users.noreply.github.com>
2024-07-27 05:23:35 +0000

    Object Constructor (singleton)

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -27,7 +27,8 @@ const regularUser = {
 
 //Object Merge
 
-const obj1 = {1: "a", 2: "b"}
+//const obj1 = {1: "a", 2: "b"}
+//obj1 //go to browser and show more property in prototype
 const obj2 = {3: "s", 4: "b"}
 
 // const obj3 = {obj1, obj2}
@@ -37,4 +38,30 @@ const obj2 = {3: "s", 4: "b"}
 //const obj3 = Object.assign({}, obj1, obj2)
 
 const obj3 = {...obj1, ...obj2}
-console.log(obj3);
+//console.log(obj3);
+
+//database value - array of object
+const user = [
+    {
+        id: 1,
+        email: "s@gmail.com"
+    },
+    {
+        id: 2,
+        email: "sb@gmail.com"
+    },
+    {
+        id: 3,
+        email: "sonu@gmail.com"
+    }
+]
+
+//console.log(user[1].email);
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser)); //arrays
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+

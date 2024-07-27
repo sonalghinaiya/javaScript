33a2cc5081dd7aa49be72a8ea53b5c01402b03be
Sonal Ghinaiya <147973094+sonalghinaiya@users.noreply.github.com>
2024-07-27 14:10:33 +0000

    Function

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,33 @@
+// console.log("S");
+// console.log("O");
+// console.log("N");
+// console.log("U");
+
+function myName() {
+    console.log("S");
+    console.log("O");
+    console.log("N");
+    console.log("U");
+}
+//myName()
+
+function addTwoNumbers(number1, number2){
+    console.log(number1 + number2);
+}
+
+// addTwoNumbers(1, 3) //4
+
+// const result = addTwoNumbers(30, 13)
+// console.log(result); //undefined what function rerturn 
+
+function addTwoNumbers(number1, number2){
+    // let result = number1 + number2
+    // return result
+    return number1 + number2
+}
+
+//return than store any variable
+const result = addTwoNumbers(30, 13)
+//console.log("Result Is:", result); 
+
+function loginUpper = 

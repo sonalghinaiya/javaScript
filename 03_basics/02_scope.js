9cfd593f59a195da93600264f6d7d4fadf90f595
Sonal Ghinaiya <147973094+sonalghinaiya@users.noreply.github.com>
2024-07-28 11:54:05 +0000

    Scope

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -9,17 +9,55 @@ if(true){
     let a = 10
     const b = 20
 
-    console.log("Inner:",a);
+    //console.log("Inner:",a);
     //c = 30
     //var c = 30
 } 
 
-console.log("Outer:",a);
+//console.log("Outer:",a);
 // console.log(b);
 //console.log(c); //30
 
 
 // ******************* Nested Scope ************************************
 function one(){
-    
+    const username = "Sonal"
+
+    function two(){
+        const websites = "Youtube"
+        //console.log(username);
+    }
+    //console.log(websites);//error
+
+    two()
 }
+
+one()
+
+if(true){
+    const username = "Sonu"
+    if(username === "Sonu"){
+        const websites = " Youtube"
+        //console.log(username + websites);
+    } 
+    //console.log(websites); //error
+}
+//console.log(username); //error
+
+
+//*******************************Mini Hoisting ***********************************
+
+console.log(addOne(5))
+
+function addOne(num){
+    return num + 1
+}
+
+
+//variable
+//console.log(addTwo(5)) //not use before declaration
+
+const addTwo = function(num){
+    return num + 2
+}
+

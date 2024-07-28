5e7e630c929a2e4d66a2806804e531374a8132a9
Sonal Ghinaiya <147973094+sonalghinaiya@users.noreply.github.com>
2024-07-28 02:39:21 +0000

    Function

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -30,4 +30,46 @@ function addTwoNumbers(number1, number2){
 const result = addTwoNumbers(30, 13)
 //console.log("Result Is:", result); 
 
-function loginUpper = 
+function loginUser(username){
+    if(username === undefined){
+        console.log("Please enter userName");
+        return
+    }
+    return `${username} Just logged in`
+}  
+// console.log(loginUser("Sonal"));
+// console.log(loginUser()); //undefined
+
+//******** shopping cart (e-commerce) don't know how much arguments came********* 
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+ //val1 :100 val2:200 num1:500, 1000
+console.log(calculateCartPrice(100, 200, 500, 1000));
+
+//object pass in function
+const user = {
+    username: "Sonal",
+    price: 199
+    //prices: 199 undefined
+}
+
+function handleObject(anyObject){
+    console.log(`Username is ${anyObject.username} and Price is ${anyObject.price}`);
+}
+
+//handleObject(user)
+handleObject({
+    username: "Sam",
+    price: 499
+})
+
+//Array pass in Function
+const myarray = [100, 200, 600, 400]
+
+function returnsecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnsecondValue(myarray))
+console.log(returnsecondValue([200, 400, 600, 800]));

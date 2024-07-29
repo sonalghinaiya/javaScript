60aaaeda4e59e3aaf9a70d274d273fcdd503a12a
Sonal Ghinaiya <147973094+sonalghinaiya@users.noreply.github.com>
2024-07-29 12:45:11 +0000

    For

--- 05_iteration/one.js
+++ 05_iteration/one.js
@@ -19,8 +19,26 @@ for(let i = 1; i <= 10 ; i++){
 
 //Array
 let myArray = ["bheem", "nobita", "suniyo"]
-
+//console.log(myArray.length)
 for (let index = 0; index < myArray.length; index++) {
     const element = myArray[index];
-    console.log(element);
+    //console.log(element);
+}
+
+//break and continue
+
+for (let i = 1; i <= 20; i++) {
+    if(i == 5){
+        //console.log(`Detected 5`);
+        break
+    }
+    //console.log(`Value of i is ${i}`);
+}
+
+for (let i = 1; i <= 20; i++) {
+    if(i == 5){
+        console.log(`Detected 5`);
+        continue
+    }
+    console.log(`Value of i is ${i}`);
 }

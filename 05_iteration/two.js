ec0c446f1ebe985f8f4d8758b8175916b2fa9b5f
Sonal Ghinaiya <147973094+sonalghinaiya@users.noreply.github.com>
2024-07-29 13:24:05 +0000

    While and Do While

---
+++ 05_iteration/two.js
@@ -0,0 +1,22 @@
+//while
+
+// let i = 0
+// while (i <= 10) {
+//     console.log(`Value of i is ${i}`);
+//     i = i + 2
+// }
+
+let myArray = ["Dora", "Nobi", "Bheem"]
+let i = 0
+while(i < myArray.length){
+    //console.log(myArray[i]);
+    i++
+}
+
+//do-while
+// let score = 10 //10
+let score = 0
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);

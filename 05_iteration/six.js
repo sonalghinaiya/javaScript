928ec4f7668b1cc0b4aaa9b3e3b9a5260b67e445
Sonal Ghinaiya <147973094+sonalghinaiya@users.noreply.github.com>
2024-07-29 17:44:41 +0000

    #

---
+++ 05_iteration/six.js
@@ -0,0 +1,10 @@
+//const coding = ["js", "ruby", "java", "python", "cpp"]
+
+// const values = coding.forEach((item) => {
+//     console.log(item)
+//     return item //undefined because forEach not return anything
+// })
+//console.log(values); //undefined
+
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+ 

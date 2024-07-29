c2fdd94684e007a01c16d44be2c9904ac2ca6620
Sonal Ghinaiya <147973094+sonalghinaiya@users.noreply.github.com>
2024-07-29 12:08:30 +0000

    For

--- 05_iteration/one.js
+++ 05_iteration/one.js
@@ -2,6 +2,25 @@
 
 for (let i = 0; i <= 10; i++) {
     const element = i;
-    console.log(element)
+    if(element == 5){
+        //console.log("5 is best number")
+    }
+    //console.log(element)
 }
 //console.log(element); //error
+
+for(let i = 1; i <= 10 ; i++){
+    //console.log(`Outer Loop Value:${i}`);
+    for (let j = 1; j <= 10; j++) {
+        //console.log(`Inner Loop Value:${j} and Outer Loop Value:${i}`);
+        //console.log(i + '*' + j + '=' + i*j);
+    }
+}
+
+//Array
+let myArray = ["bheem", "nobita", "suniyo"]
+
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    console.log(element);
+}

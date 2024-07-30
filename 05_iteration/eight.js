69740f94db97d59c2fa326891c6d66456a28d400
Sonal Ghinaiya <147973094+sonalghinaiya@users.noreply.github.com>
2024-07-30 16:48:18 +0000

    Reduce

--- 05_iteration/eight.js
+++ 05_iteration/eight.js
@@ -1,10 +1,30 @@
 //reduce
 
 const myNumbs = [1, 2, 3, 4, 5]
-const myTotal = myNumbs.reduce(function (acc, currval){
-    console.log(`Accumulator: ${acc} and Current Value:${currval}`);
-    return acc + currval
+
+const myTotal = myNumbs.reduce(function (acc, curval){
+    console.log(`Accumulator: ${acc} and Current Value: ${curval}`);
+    return acc + curval
 },0)
+console.log(myTotal)
 
+const sum = myNumbs.reduce( (accumulator, currentValue) => accumulator + currentValue,0)
+console.log(sum);
 
-console.log(myTotal);
+const shopping_cart = [
+    {
+        itemName: "JS Course",
+        price: 299
+    },
+    {
+        itemName: "Java Course",
+        price: 2999
+    },
+    {
+        itemName: "React Course",
+        price: 799
+    }
+]
+
+const totalPrice = shopping_cart.reduce( (acc, item) => acc+ item.price,0)
+console.log(totalPrice);
